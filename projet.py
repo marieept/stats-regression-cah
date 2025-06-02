45bf4782d76f0cf50cba4ab8fa48aa62484f9349
PouLoulou18 <louis.robillard18@gmail.com>
2025-06-02 16:12:07 +0200

    Update

--- projet.py
+++ projet.py
@@ -63,15 +63,15 @@ b1 = np.sum((x - x_moy) * (y - y_moy)) / np.sum((x - x_moy)**2)
 b0 = y_moy - b1 * x_moy
 
 # Modèle de prédictions
-y_estime = b0 + b1 * x
+y_pred = b0 + b1 * x
 
 
 # 2.3 Coefficient de Détermination R²
 
 # R²
-SCE = np.sum((y - y_estime) ** 2)
+SCE = np.sum((y - y_pred) ** 2)
 SCT = np.sum((y - y_moy) ** 2)
-SCR = np.sum((y_estime - y_moy) ** 2)
+SCR = np.sum((y_pred - y_moy) ** 2)
 R2 = 1 - SCE / SCT
 
 
@@ -79,7 +79,7 @@ R2 = 1 - SCE / SCT
 
 # Affichage
 plt.scatter(x, y, label='Points')
-plt.plot(x, y_estime, color='red', label=f'Droite de régression: y = {b0:.2f} + {b1:.2f}x')
+plt.plot(x, y_pred, color='red', label=f'Droite de régression: y = {b0:.2f} + {b1:.2f}x')
 plt.legend()
 plt.title(f'Droite de régression linéaire simple (R² = {R2:.2f})')
 plt.xlabel("x")
@@ -95,8 +95,6 @@ print("Graphique enregistré dans le fichier figure_2_1.jpg")
 #==================================
 
 #1. Résidus et somme des carrés des erreurs (SCE)
-y_pred = b0 + b1*x
-
 e = y - y_pred
 
 SCE=0
